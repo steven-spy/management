b77b8818e66e98210fe250c5524269de6ff51e45
steven-spy <1248788505@qq.com>
2020-07-01 16:17:24 +0800

    practice

---
+++ Web/proxy.conf.js
@@ -0,0 +1,19 @@
+const preventBrowserCalls = function (req, res, proxyOptions) {
+  if (req.headers.accept && req.headers.accept.indexOf('html') !== -1) {
+    return '/index.html';
+  }
+  req.headers['X-Custom-Header'] = 'yes';
+};
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      '/User',
+    ],
+    target: 'http://localhost:8090',
+    secure: false,
+    bypass: preventBrowserCalls,
+    changeOrigin: true
+  },
+];
+module.exports = PROXY_CONFIG;
