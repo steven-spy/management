4763167cbe2e39bb8a8f69a3b8d6a563454e0b4a
ansin/sunpengyu <1248788505@qq.com>
2021-03-23 14:37:05 +0800

    添加注释信息

--- management/src/main/java/com/software/management/ManagementApplication.java
+++ management/src/main/java/com/software/management/ManagementApplication.java
@@ -4,9 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.core.SpringVersion;
 
-@SpringBootApplication
+/**
+ * @author Steven
+ */
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.software.management.dao.mapper")
 public class ManagementApplication {
 
